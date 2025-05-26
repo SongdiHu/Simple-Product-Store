3e20b61df90840f63437b89292c765ee59880bd4
Sodi4 <hus@myumanitoba.ca>
2025-05-26 10:36:53 -0500

    frontend initialized

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -25,11 +25,12 @@ export const createProduct = async(req, res) => {
 export const deleteProduct = async(req, res) => {
   const { id } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ success: false, message: 'Invalid product ID' });
+  }
+
   try {
     const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json({ success: false, message: 'Product not found' });
-    }
     res.status(200).json({ success: true, data: product });
   } catch (error) {
     console.error('Error deleting product:', error.message);
--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import productRoutes from './routes/product.route.js';
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
@@ -16,7 +17,7 @@ app.use("/api/products", productRoutes);
 
 app.listen(5000, () => {
   connectDB();
-  console.log('Server is running on port 5000, at http://localhost:5000');
+  console.log('Server is running on port ' + PORT + ', at http://localhost:' + PORT);
 });
 
 //uBUS92ARGzKAt3QG
